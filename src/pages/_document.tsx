972e961e4d97f939060586f64fd01602fe60e68a
Kunal Bagaria <kb24x7@gmail.com>
2023-01-22 16:15:13 +0530

    add: blocks content

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- src/sections/blocks.tsx
+++ src/sections/blocks.tsx
@@ -5,8 +5,13 @@ function Blocks() {
   return (
     <section className={styles.blocksSection}>
       <div className={styles.blocksText}>
-        <p className={concatClassNames("text weight-700 size-48 gray-800 nm", styles.blocksHeading)}>{"The building blocks you need to build sticky dApps"}</p>
-
+        <div className={styles.blocksHeading}>
+          <p className="text weight-700 size-48 gray-800 nm">{"The building blocks you need to build sticky dApps"}</p>
+        </div>
+        <div className={styles.blocksSubHeading}>
+          <p className="text weight-400 size-24 gray-500">Gum empowers developers with a whole host of tools to build rich and seamless social apps that people stick to</p>
+          <p className={concatClassNames("text weight-500 size-24 inter blue", styles.docsLink)}>Read Docs <span>{"->"}</span></p>
+        </div>
       </div>
     </section>
   )
