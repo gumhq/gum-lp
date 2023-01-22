ceb5341fdc2ecaa8988414eafefed238794f6308
Kunal Bagaria <kb24x7@gmail.com>
2023-01-22 16:34:31 +0530

    add: navbar

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,9 +1,10 @@
 import styles from '@/styles/Home.module.scss';
+import { Navbar } from './navbar';
 
 function Hero() {
   return (
     <section className={styles.heroSection}>
-
+      <Navbar />
     </section>
   )
 };
---
+++ src/sections/navbar.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import styles from '@/styles/Home.module.scss';
+import logo from '@/images/gum.svg';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navbarContent}>
+        <Image src={logo} alt="" />
+        <div className={styles.navLinks}>
+          <a href="#">Docs</a>
+          <a href="#">Blog</a>
+          <a href="#">Community</a>
+        </div>
+        <button className={styles.navButton}>Start Building</button>
+      </div>
+    </nav>
+  );
+}
+
+export { Navbar }
