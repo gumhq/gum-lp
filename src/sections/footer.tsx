de50c059043c32c291626f62a1ee6d21483b8b65
Kunal Bagaria <kb24x7@gmail.com>
2023-01-24 16:41:23 +0530

    add: social icons to footer

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -2,13 +2,32 @@ import Image from 'next/image';
 import logo from '@/images/gum.svg';
 import styles from '@/styles/Home.module.scss';
 
+// Icon Imports
+import WordcelIcon from '@/components/icons/Wordcel';
+import DiscordIcon from '@/components/icons/Discord';
+import TwitterIcon from '@/components/icons/Twitter';
+
+const socials = [
+  { link: 'https://twitter.com/wordcel_club', icon: TwitterIcon },
+  { link: 'https://discord.gg/tCswbSK5W2', icon: DiscordIcon },
+  { link: "https://wordcelclub.com/wordcelclub.sol", icon: WordcelIcon },
+];
+
 function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <Image src={logo} alt="" />
-        <div className={styles.footerLinks}>
-
+        <div className={styles.socials}>
+          {socials.map((social) => (
+            <a
+              href={social.link}
+              rel="noopener noreferrer"
+              target="_blank"
+              key={social.link}>
+                {<social.icon color="#1E2833" />}
+            </a>
+          ))}
         </div>
       </div>
     </footer>
