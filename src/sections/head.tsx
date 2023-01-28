400255f8d41a0036f089c3b45b54dc516ecb40a6
Kunal Bagaria <kb24x7@gmail.com>
2023-01-28 23:09:44 +0530

    update: social appearance

--- src/sections/head.tsx
+++ src/sections/head.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 
-const OG_IMAGE = 'https://u3thexwfftkkw6thj7ufv2bdv5spd76woutboy7pvxf5dgyzjqyq.arweave.net/puZyXsUs1Kt6Z0_oWugjr2Tx_9Z1Jhdj763L0ZsZTDE';
+const OG_IMAGE = 'https://n6hf2tsvd5arodir7eom3qebwnl5fvstuwezbamh6f7wdndzzbqq.arweave.net/b45dTlUfQRcNEfkczcCBs1fS1lOliZCBh_F_YbR5yGE';
 
 function DefaultHead () {
   return (
     <Head>
       <title>{'Gum'}</title>
-      <meta name='description' content={'Build Decentralized Social Apps That Scale'} />
+      <meta name='description' content={'Solana Social Legos: Build sticky apps with ease'} />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta name="theme-color" content="white" />
       <link rel='icon' href='/favicon.png' />
@@ -14,12 +14,12 @@ function DefaultHead () {
       <meta property='og:type' content={'website'} />
       <meta property='og:url' content={'https://gum.fun'} />
       <meta property='og:title' content={'Gum'} />
-      <meta property='og:description' content={'Build Decentralized Social Apps That Scale'} />
+      <meta property='og:description' content={'Solana Social Legos: Build sticky apps with ease'} />
       <meta property='og:image' content={OG_IMAGE} />
       <meta property='twitter:card' content='summary_large_image' />
       <meta property='twitter:url' content={'https://gum.fun'} />
       <meta property='twitter:title' content={'Gum'} />
-      <meta property='twitter:description' content={'Build Decentralized Social Apps That Scale'} />
+      <meta property='twitter:description' content={'Solana Social Legos: Build sticky apps with ease'} />
       <meta property='twitter:image' content={OG_IMAGE} />
     </Head>
   );
